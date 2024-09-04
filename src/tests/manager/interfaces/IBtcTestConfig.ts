12997096b8cd52f527b683bd0119b3e39d713017
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-09-03 20:10:39 -0400

    Support cjs

--- src/tests/manager/interfaces/IBtcTestConfig.ts
+++
@@ -1,6 +0,0 @@
-import { IConfig, IConfigTemplate } from '@btc-vision/bsi-common';
-import { RPCConfig } from '../../../rpc/interfaces/RPCConfig.js';
-
-export interface IBtcTestConfig extends IConfig<IConfigTemplate> {
-    BLOCKCHAIN: RPCConfig;
-}
